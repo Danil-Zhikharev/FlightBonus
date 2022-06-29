34d605e009025d002a0d2867f6edb4aac958c430
Danil-Zhikharev <Dane99mail.rugit config --global user.name Danil-Zhikharev$  git config --global user.email Dane99mail.ru>
2022-06-29 21:19:59 +0500

    Last_commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20;
+        int ticket = 5000;
+        int miles = ticket / amount;
+        System.out.println("Начисленные милли: " + miles);
+    }
+}
